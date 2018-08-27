5d16b7b806b7bec7f24802e8c9d87d73a99de2a3
frmnjn <firmanbudis@gmail.com>
2018-08-27 12:57:39 +0700

    update registrasi perijinan form field

--- admin/src/views/AdminTPU/RegistrasiPerijinanMakam/RegistrasiPerijinanMakam.js
+++ admin/src/views/AdminTPU/RegistrasiPerijinanMakam/RegistrasiPerijinanMakam.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Bar, Line } from 'react-chartjs-2';
-import usersData from '../../../views/Users/UsersData';
+import DatePicker from 'react-datepicker';
+import Select from 'react-select';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
 import {
   Badge,
   Button,
@@ -41,6 +44,42 @@ class RegistrasiPerijinanMakam extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.makam_items = this.makam_items.bind(this);
+    this.handleDate = this.handleDate.bind(this);
+
+    this.state = {
+      startDate: moment(),
+
+      makam: [],
+      makamitems:[],
+      list: [],
+
+      nama: "",
+      alamat_terakhir: "",
+      tanggal_wafat: '',
+      status: "",
+      id_makam: "",
+      kode_makam: "",
+      nama_ahli_waris: "",
+      alamat_ahli_waris: "",
+      nik_ahli_waris: "",
+      kontak_ahli_waris: "",
+      activetanggal_wafat: '',
+
+      selectvalue: null,
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/makam/view?token=' + sessionStorage.getItem('token') + '&id_user=' + sessionStorage.getItem('id_user'))
+      .then(response => response.json())
+      .then(
+        (result) => {
+          this.setState({
+            makam: result,
+            isLoaded: true
+          }); //console.log(result);
+        })
   }
 
   onchange(e) {
@@ -51,11 +90,87 @@ class RegistrasiPerijinanMakam extends Component {
     data.append(e.target.name, e.target.value);
   }
 
-  handleSubmit = event =>  {
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  makam_items() {
+    var newitem = [];
+    this.state.makam.map((items) => {
+      newitem = newitem.concat({ value: items.id_makam, label: items.kode_makam })
+    })
+
+    return newitem
+  }
+
+  handleSubmitCreate = event => {
     event.preventDefault();
+    fetch('http://localhost:8000/api/penghuni_makam/create?token=' + sessionStorage.getItem('token'), {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        nama: this.state.nama,
+        alamat_terakhir: this.state.alamat_terakhir,
+        tanggal_wafat: this.state.tanggal_wafat.format().substring(0, 10),
+        status: this.state.status,
+        id_makam: this.state.id_makam,
+        nama_ahli_waris: this.state.nama_ahli_waris,
+        alamat_ahli_waris: this.state.alamat_ahli_waris,
+        nik_ahli_waris: this.state.nik_ahli_waris,
+        kontak_ahli_waris: this.state.kontak_ahli_waris,
+        activetanggal_wafat: '',
+      })
+    }).then(
+      this.fetchall
+    ).then(
+      this.setState({
+        primary: !this.state.primary
+      })
+    )
+    alert("Data penghuni baru berhasil ditambahkan!");
+  }
+
+  handleDate(date) {
+    this.setState({
+      activetanggal_wafat: date,
+      tanggal_wafat: date,
+    });
+  }
+
+  handleSelect = (selectedOption) =>{
+    this.setState({ selectedOption,activeid_makam: selectedOption.value,id_makam: selectedOption.value});
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    data.append('nama_almarhum',this.state.nama);
+    data.append('nama_pewaris',this.state.nama_ahli_waris);
+
     for (var value of data.values()) {
       console.log(value);
     }
+
+    fetch('http://localhost:8000/api/penghuni_makam/create?token=' + sessionStorage.getItem('token'), {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        nama: this.state.nama,
+        alamat_terakhir: this.state.alamat_terakhir,
+        tanggal_wafat: this.state.tanggal_wafat.format().substring(0, 10),
+        status: this.state.status,
+        id_makam: this.state.id_makam,
+        nama_ahli_waris: this.state.nama_ahli_waris,
+        alamat_ahli_waris: this.state.alamat_ahli_waris,
+        nik_ahli_waris: this.state.nik_ahli_waris,
+        kontak_ahli_waris: this.state.kontak_ahli_waris,
+      })
+    })
 
     fetch('http://localhost:8000/api/upload?token=' + sessionStorage.getItem('token'), {
       method: 'POST',
@@ -83,31 +198,59 @@ class RegistrasiPerijinanMakam extends Component {
               <CardBody>
                 {/* <hr></hr> */}
                 <Col xs="4">
-                <form onSubmit={this.handleSubmit}>
-                <div class="form-group">
-                  <label>
-                    Nama Almarhum:
-                  </label>
-                  <input type="text" onChange={this.onchangeText} class="form-control" name="nama_almarhum" placeholder="Nama Almarhum"/>
-                  <br />
-                  <label>
-                    Nama Pewaris:
-                  </label>
-                  <input type="text" onChange={this.onchangeText} class="form-control" name="nama_pewaris" placeholder="Nama Pewaris"/>
-                  <br />
-                  <label for="ktp_pewaris">
-                    KTP Pewaris:
+                  <form onSubmit={this.handleSubmit}>
+                    <div class="form-group">
+
+                      <label>Nama Penghuni Makam</label>
+                      <input type="text" className="form-control" name="nama" placeholder="Nama Penghuni" onChange={this.handleChange}></input>
+                      <label>Alamat Terakhir</label>
+                      <input type="text" className="form-control" name="alamat_terakhir" placeholder="Alamat Terakhir" onChange={this.handleChange}></input>
+                      <br />
+                      <label>Tanggal Wafat</label>
+                      <DatePicker dateFormat="DD/MM/YYYY" selected={this.state.tanggal_wafat} onChange={this.handleDate} />
+                      <br />
+                      <label>Status</label>
+                      <Input type="select" className="form-control" name="status" onChange={this.handleChange}>
+                        <option value='Diperpanjang'>Diperpanjang</option>
+                        <option value='Expired'>Expired</option>
+                        <option value='Ditimpa'>Ditimpa</option>
+                      </Input>
+                      <label>Pilih Nomor Makam (Kode Makam)</label>
+                      <Select
+                        value={this.state.selectedOption}
+                        onChange={this.handleSelect}
+                        options={this.makam_items()}
+                      />
+                      {/* <Input type="select" className="form-control" name="id_makam" onChange={this.handleChange}>
+                            {this.state.makam.map((items) => {
+                              return (
+                                <option value={items.id_makam}>{items.kode_makam}</option>
+                              )
+                            }
+                            )}
+                          </Input> */}
+                      <label>Nama Ahli Waris</label>
+                      <input type="text" className="form-control" name="nama_ahli_waris" placeholder="Nama Ahli Waris" onChange={this.handleChange}></input>
+                      <label>Alamat Ahli Waris</label>
+                      <input type="text" className="form-control" name="alamat_ahli_waris" placeholder="Alamat Ahli Waris" onChange={this.handleChange}></input>
+                      <label>NIK Ahli Waris</label>
+                      <input type="text" className="form-control" name="nik_ahli_waris" placeholder="NIK Ahli Waris" onChange={this.handleChange}></input>
+                      <label>Kontak Ahli Waris</label>
+                      <input type="text" className="form-control" name="kontak_ahli_waris" placeholder="Kontak Ahli Waris" onChange={this.handleChange}></input>
+                      {/* <input type="submit" className="form-control btn btn-primary" value="Submit"></input> */}
+                      <label for="ktp_pewaris">
+                        KTP Pewaris:
                 <input type="file" onChange={this.onchange} class="form-control-file" name="ktp_pewaris" />
-                  </label>
-                  <br />
-                  <label for="surat_kematian">
-                    Surat Kematian:
+                      </label>
+                      <br />
+                      <label for="surat_kematian">
+                        Surat Kematian:
                 <input type="file" onChange={this.onchange} class="form-control-file" name="surat_kematian" />
-                  </label>
-                  <br />
-                  <input type="submit" value="Submit"></input>
-                </div>
-                </form>
+                      </label>
+                      <br />
+                      <input type="submit" value="Submit"></input>
+                    </div>
+                  </form>
                 </Col>
               </CardBody>
             </Card>
