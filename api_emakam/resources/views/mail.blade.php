a2f0c31800b553a90c66f8c2f61dad24e98f5588
fadhlanm <fadhlanmuhamad@gmail.com>
2018-09-04 08:24:59 +0700

    mail formatted

--- api_emakam/resources/views/mail.blade.php
+++ api_emakam/resources/views/mail.blade.php
@@ -4,7 +4,13 @@
 	<title>Document</title>
 </head>
 <body>
-	<h1>{{$msg}}</h1>
+	<p>{{$msg}}<p>
+	<p>=================================</p>
+	<h3>Dinas Perumahan dan Kawasan Pemukiman Malang</h3>
+	<p>Jl. Bingkil No.37, Ciptomulyo, Sukun, Kota Malang, Jawa Timur 65148</p>
+
+	<p>=================================</p>
 	<p>no reply message</p>
+
 </body>
 </html>
