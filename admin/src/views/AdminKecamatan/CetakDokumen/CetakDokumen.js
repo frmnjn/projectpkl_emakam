8b852327966ba86b5a40874c23475d3a2be52ffb
fadhlanm <fadhlanmuhamad@gmail.com>
2018-09-20 18:19:41 +0700

    cetak pdf fixed

--- admin/src/views/AdminKecamatan/CetakDokumen/CetakDokumen.js
+++ admin/src/views/AdminKecamatan/CetakDokumen/CetakDokumen.js
@@ -42,6 +42,8 @@ class CetakDokumen extends Component {
 
     this.cetak_dokumen = this.cetak_dokumen.bind(this);
     this.modalnosuratclose = this.modalnosuratclose.bind(this);
+    this.get_tanggal_sekarang = this.get_tanggal_sekarang.bind(this);
+
     // this.get_active_row = this.get_active_row.bind(this);
 
     this.state = {
@@ -87,7 +89,7 @@ class CetakDokumen extends Component {
         id: id,
         status: 'Proses Selesai',
         no_surat_perizinan: this.state.no_surat,
-        tanggal_surat_permohonan: this.get_tanggal_sekarang()
+        tanggal_surat_perizinin: this.get_tanggal_sekarang()
       })
     }).then((response) => response.json())
       .then((responseJson) => {
@@ -112,25 +114,50 @@ class CetakDokumen extends Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
-  cetak_dokumen = event => {
+  nomor_surat = event => {
     event.preventDefault();
     this.update_no_surat_perizinan(this.state.activenosurat.id);
-    const url = 'http://localhost:8000/api/dokumen/cetak_surat_perizinan?token=' + sessionStorage.getItem('token')
-    +'&tanggal_sekarang='+this.get_tanggal_sekarang()            
-    +'&nama_ahli_waris='+this.state.activenosurat.nama_pewaris
-    +'&alamat_ahli_waris='+this.state.activenosurat.alamat_ahli_waris
-    +'&nama_almarhum='+this.state.activenosurat.nama_almarhum
-    +'&ttl_almarhum='+this.state.activenosurat.tanggal_lahir_alm
-    +'&jenis_kelamin_almarhum='+this.state.activenosurat.jenis_kelamin
-    +'&tpu_almarhum='+this.state.activenosurat.nama_tpu
-    +'&tanggal_pemakaman='+this.state.activenosurat.tanggal_pemakaman
-    +'&blok_almarhum='+this.state.activenosurat.kode_blok
-    +'&no_surat_perizinan='+this.state.no_surat
-    +'&no_surat_permohonan='+this.state.activenosurat.no_surat_permohonan
-    +'&tanggal_surat_permohonan='+this.state.activenosurat.tanggal_surat_permohonan
-    window.location = url;
-
     this.modalnosuratclose();
+  }
+
+  surat_permohonan = (items) =>{
+    if(items.no_surat_perizinan!=null){
+      const url = 'http://localhost:8000/api/dokumen/cetak_surat_permohonan?token=' + sessionStorage.getItem('token')
+      + '&tanggal_sekarang=' + items.tanggal_surat_permohonan
+      + '&nama_ahli_waris=' + items.nama_pewaris
+      + '&alamat_ahli_waris=' + items.alamat_ahli_waris
+      + '&tanggal_wafat=' + items.tanggal_wafat
+      + '&nama_almarhum=' + items.nama_almarhum
+      + '&ttl_almarhum=' + items.tanggal_lahir_alm
+      + '&ttl_ahli_waris=' + items.tgllhr_ahli_waris
+      + '&jenis_kelamin_almarhum=' + items.jenis_kelamin
+      + '&tpu_almarhum=' + items.nama_tpu
+      + '&alamat_almarhum=' + items.alamat_terakhir
+      + '&tanggal_pemakaman=' + items.tanggal_pemakaman
+      + '&blok_almarhum=' + items.kode_blok
+      + '&no_surat_permohonan=' + items.no_surat_permohonan
+      window.location = url;
+    }
+  }
+
+  cetak_dokumen = (items) => {
+    // event.preventDefault();
+    if(items.no_surat_perizinan!=null){
+      const url = 'http://localhost:8000/api/dokumen/cetak_surat_perizinan?token=' + sessionStorage.getItem('token')
+      +'&tanggal_sekarang='+items.tanggal_surat_perizinin          
+      +'&nama_ahli_waris='+items.nama_pewaris
+      +'&alamat_ahli_waris='+items.alamat_ahli_waris
+      +'&nama_almarhum='+items.nama_almarhum
+      +'&ttl_almarhum='+items.tanggal_lahir_alm
+      +'&jenis_kelamin_almarhum='+items.jenis_kelamin
+      +'&tpu_almarhum='+items.nama_tpu
+      +'&tanggal_pemakaman='+items.tanggal_pemakaman
+      +'&blok_almarhum='+items.kode_blok
+      +'&no_surat_perizinan='+items.no_surat_perizinan
+      +'&no_surat_permohonan='+items.no_surat_permohonan
+      +'&tanggal_surat_permohonan='+items.tanggal_surat_permohonan
+      window.location = url;
+    }
   }
 
   nosuratmodal(items) {
@@ -162,7 +189,7 @@ class CetakDokumen extends Component {
         <Modal isOpen={this.state.nosuratmodal} toggle={this.modalnosuratclose} className={'modal-Large ' + this.props.className}>
             <ModalHeader toggle={this.modalnosuratclose}>No Surat</ModalHeader>
             <ModalBody>
-              <form className="form-group" onSubmit={this.cetak_dokumen}>
+              <form className="form-group" onSubmit={this.nomor_surat}>
                 <div class="form-group">
                 <label>No Surat</label>
                 <input type="text" className="form-control" onChange={this.handleChange} name="no_surat"></input>
@@ -185,7 +212,9 @@ class CetakDokumen extends Component {
                     columns={[
                       { accessor: 'id', show: false },
                       { accessor: 'nama_almarhum', show: false },
+                      { accessor: 'no_surat_perizinan', show: false },
                       { accessor: 'tanggal_surat_permohonan', show: false },
+                      { accessor: 'tanggal_surat_perizinin', show: false },
                       { accessor: 'no_surat_permohonan', show: false },
                       { accessor: 'nik_ahli_waris', show: false },
                       { accessor: 'alamat_ahli_waris', show: false },
@@ -215,11 +244,29 @@ class CetakDokumen extends Component {
                         show: false
                       },
                       {
-                        Header: 'Actions',
+                        Header: 'Nomor Surat',
                         // accessor: 'status', // String-based value accessors!
                         Cell: row => (
                           <div>
-                            <Button color="info" onClick={() => this.nosuratmodal(row.row)} className="mr-1">Cetak Dokumen</Button>
+                            <Button color="info" onClick={() => this.nosuratmodal(row.row)} className="mr-1">Atur</Button>
+                          </div>
+                        )
+                      },
+                      {
+                        Header: 'Cetak Surat Permohonan',
+                        // accessor: 'status', // String-based value accessors!
+                        Cell: row => (
+                          <div>
+                            <Button color="info" onClick={() => this.surat_permohonan(row.row)} className="mr-1">Cetak Dokumen</Button>
+                          </div>
+                        )
+                      },
+                      {
+                        Header: 'Cetak Surat Izin',
+                        // accessor: 'status', // String-based value accessors!
+                        Cell: row => (
+                          <div>
+                            <Button color="info" onClick={() => this.cetak_dokumen(row.row)} className="mr-1">Cetak Dokumen</Button>
                           </div>
                         )
                       },
